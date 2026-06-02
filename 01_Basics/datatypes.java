226efbc1e719b4dbb2b393368896ec43c3df58c0
anapearl06 <shuklaananya762@gmail.com>
2026-06-02 19:56:43 +0530

    DataTypes

---
+++ 01_Basics/datatypes.java
@@ -0,0 +1,38 @@
+public class datatypes {
+    public static void main() {
+
+        // Whole number
+        int age = 20;
+
+        // Decimal number
+        double height = 5.6;
+
+        // Single character
+        char grade = 'A';
+
+        // True or false value
+        boolean isStudent = true;
+
+        // Text value
+        String name = "Ananya";
+
+        // Small integer value
+        byte marks = 95;
+
+        // Large whole number
+        long phoneNumber = 9876543210L;
+
+        // Decimal value with less precision
+        float percentage = 89.5f;
+
+        // Printing all values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+        System.out.println("Marks: " + marks);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Percentage: " + percentage);
+    }
+}
