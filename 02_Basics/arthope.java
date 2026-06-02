15c5a6f970c5508db8693a9743bc1c4bd4b7d953
anapearl06 <shuklaananya762@gmail.com>
2026-06-02 20:07:08 +0530

    Arithetic Operators

---
+++ 02_Basics/arthope.java
@@ -0,0 +1,30 @@
+public class arthope
+{
+    public static void main()
+    {
+        int a = 20;
+        int b = 10;
+
+        // Addition
+        int sum = a + b;
+
+        // Subtraction
+        int difference = a - b;
+
+        // Multiplication
+        int product = a * b;
+
+        // Division
+        int quotient = a / b;
+
+        // Modulus (Remainder)
+        int remainder = a % b;
+
+        // Printing results
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
