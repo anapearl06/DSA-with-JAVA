bd3591ae7f993fd13b614e61f4f808449496b56d
anapearl06 <shuklaananya762@gmail.com>
2026-06-02 19:46:11 +0530

    Variables

---
+++ 01_Basics/variables.java
@@ -0,0 +1,26 @@
+public class variables {
+    public static void main()
+    {
+        // Integer variable
+        int age = 20;
+
+        // Decimal number variable
+        double height = 5.6;
+
+        // Character variable
+        char grade = 'A';
+
+        // Boolean variable
+        boolean isStudent = true;
+
+        // String variable
+        String name = "Ananya";
+
+        // Printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
