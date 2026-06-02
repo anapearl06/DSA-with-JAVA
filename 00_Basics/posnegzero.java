759dd022ae567828669973f1d8aef3656fa8566c
anapearl06 <shuklaananya762@gmail.com>
2026-06-02 17:56:29 +0530

    Added positive/negative/zero checker

---
+++ 00_Basics/posnegzero.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class posnegzero {
+
+    public static void main()
+    {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        if(num > 0)
+        {
+            System.out.println("positive");
+        }
+        else if(num < 0)
+        {
+            System.out.println("negative");
+        }
+        else
+        {
+            System.out.println("zero");
+        }
+
+        sc.close();
+    }
+}
