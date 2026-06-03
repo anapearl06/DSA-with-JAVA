77cccbac8e7ff63755c815bd7b05e6e36c337bf3
anapearl06 <shuklaananya762@gmail.com>
2026-06-03 22:42:40 +0530

    updated

---
+++ Questions/number_2.java
@@ -0,0 +1,12 @@
+public class number_2 {
+    public static void main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
