68c42040af5f763d97d9a584357f09dfd77a13e9
anapearl06 <shuklaananya762@gmail.com>
2026-06-04 23:57:06 +0530

    special

---
+++ D_Conditionals/Questions/triangle_11.java
@@ -0,0 +1,22 @@
+package D_Conditionals.Questions;
+import java.util.Scanner;
+public class triangle_11
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a + b > c && a + c > b && b + c > a)
+        {
+            System.out.println("Valid Triangle");
+        }
+        else
+        {
+            System.out.println("Invalid Triangle");
+        }
+    }
+}
