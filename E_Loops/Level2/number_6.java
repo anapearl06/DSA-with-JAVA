9923ff096ed04cdcd2847b514bf3f56724c33623
anapearl06 <shuklaananya762@gmail.com>
2026-06-06 20:17:28 +0530

    Special

---
+++ E_Loops/Level2/number_6.java
@@ -0,0 +1,25 @@
+package E_Loops.Level2;
+public class number_6
+{
+    public static void main()
+    {
+        for (int num = 2; num <= 100; num++)
+        {
+            boolean isPrime = true;
+
+            for (int i = 2; i < num; i++)
+            {
+
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(num);
+            }
+        }
+    }
+}
