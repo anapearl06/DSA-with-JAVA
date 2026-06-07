0ca008511a5a7247586375df261df003477b8488
anapearl06 <shuklaananya762@gmail.com>
2026-06-07 23:31:19 +0530

    nested

---
+++ D_Conditionals/Special/nested_ifelse.java
@@ -0,0 +1,56 @@
+package D_Conditionals.Special;
+import java.util.Scanner;
+public class nested_ifelse {
+    public static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a digit (0-9): ");
+        int num = sc.nextInt();
+
+        if (num == 0) {
+
+            System.out.println("Zero");
+
+        } else if (num == 1) {
+
+            System.out.println("One");
+
+        } else if (num == 2) {
+
+            System.out.println("Two");
+
+        } else if (num == 3) {
+
+            System.out.println("Three");
+
+        } else if (num == 4) {
+
+            System.out.println("Four");
+
+        } else if (num == 5) {
+
+            System.out.println("Five");
+
+        } else if (num == 6) {
+
+            System.out.println("Six");
+
+        } else if (num == 7) {
+
+            System.out.println("Seven");
+
+        } else if (num == 8) {
+
+            System.out.println("Eight");
+
+        } else if (num == 9) {
+
+            System.out.println("Nine");
+
+        } else {
+
+            System.out.println("Invalid Digit");
+        }
+    }
+}
