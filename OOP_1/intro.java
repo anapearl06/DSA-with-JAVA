73a8845bcd0b7e0e3fae511396ef27fac0dddb56
anapearl06 <shuklaananya762@gmail.com>
2026-06-08 19:37:27 +0530

    OOP

---
+++ OOP_1/intro.java
@@ -0,0 +1,32 @@
+package OOP_1;
+public class intro {
+    public static void main() {
+        //stores 5  roll number
+        int[] numbers = new int[5];
+
+        //store 5 names
+        String[] names = new String[5];
+
+        //data of 5 students
+        //(roll number, name and marks)
+
+        int[] rollnumber = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+
+        //students
+
+        Student[] students = new Student[5];
+
+        Student Ana;
+    }
+        //create class
+        class Student {
+
+            int[] rollnumber = new int[5];
+            String[] name = new String[5];
+            float[] marks = new float[5];
+        }
+    }
+
+
