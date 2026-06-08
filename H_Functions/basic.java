878ca647169ac9afa0190dc77a95aeb0427e31cb
anapearl06 <shuklaananya762@gmail.com>
2026-06-08 22:45:12 +0530

    functions

---
+++ H_Functions/basic.java
@@ -0,0 +1,14 @@
+package H_Functions;
+public class basic
+{
+    // function
+    static void printName() {
+        System.out.println("Ananya");
+    }
+
+    public static void main(String[] args) {
+
+        // function calling
+        printName();
+    }
+}
